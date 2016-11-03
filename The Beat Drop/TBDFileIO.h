b29120f3debf689d2413dcfee37fa814ac1d3eac
Jonathan Danek <jonathan@danek.me>
2016-11-02 22:55:17 -0400

    Created File I/O Object

---
+++ The Beat Drop/TBDFileIO.h
@@ -0,0 +1,13 @@
+//
+//  TBDFileIO.h
+//  The Beat Drop
+//
+//  Created by Jonathan Danek on 11/2/16.
+//  Copyright © 2016 Jonathan Danek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TBDFileIO : NSObject
+
+@end
---
+++ The Beat Drop/TBDFileIO.m
@@ -0,0 +1,13 @@
+//
+//  TBDFileIO.m
+//  The Beat Drop
+//
+//  Created by Jonathan Danek on 11/2/16.
+//  Copyright © 2016 Jonathan Danek. All rights reserved.
+//
+
+#import "TBDFileIO.h"
+
+@implementation TBDFileIO
+
+@end
