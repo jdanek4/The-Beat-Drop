6111fc031220c59047498cba2d93854c61e2b959
Jonathan Danek <jonathan@danek.me>
2016-11-08 15:11:26 -0500

    Created Custom TableViewCell Class

---
+++ The Beat Drop/TBDTrackTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TBDTrackTableViewCell.h
+//  The Beat Drop
+//
+//  Created by Jonathan Danek on 11/3/16.
+//  Copyright © 2016 Jonathan Danek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TBDTrackTableViewCell : UITableViewCell
+
+@end
---
+++ The Beat Drop/TBDTrackTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  TBDTrackTableViewCell.m
+//  The Beat Drop
+//
+//  Created by Jonathan Danek on 11/3/16.
+//  Copyright © 2016 Jonathan Danek. All rights reserved.
+//
+
+#import "TBDTrackTableViewCell.h"
+
+@implementation TBDTrackTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
